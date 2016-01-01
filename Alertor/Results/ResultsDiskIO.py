1a0f83abaf5a588f307184884b836e887288870b
Benjamin Bourdin <benjamin.bourdin40@gmail.com>
2016-01-01 19:04:28 +0100

    - Add todo list. - Store alerts' results in Result/[current_date]/ now.

--- Alertor/Results/ResultsDiskIO.py
+++ Alertor/Results/ResultsDiskIO.py
@@ -3,6 +3,7 @@ import json
 import os
 import uuid
 import pickle
+import datetime
 
 from Alerts.Alert import Alert
 from Results.Result import Result, Interval
@@ -11,11 +12,20 @@ from CacheDiskIO import CacheDiskIO
 
 class ResultsDiskIO(CacheDiskIO):
 
+    startTimestamp = datetime.datetime.now()
+
     def __init__(self):
         super().__init__()
         self.resultPrefix = "result_"
 
         self.fullDir = os.path.join(self.fullDir, "Results")
+        currentDir = "{year:02d}{month:02d}{day:02d}_{hour:02d}{min:02d}{sec:02d}".format(year=self.startTimestamp.year,
+                                                                                          month=self.startTimestamp.month,
+                                                                                          day=self.startTimestamp.day,
+                                                                                          hour=self.startTimestamp.hour,
+                                                                                          min=self.startTimestamp.minute,
+                                                                                          sec=self.startTimestamp.second)
+        self.fullDir = os.path.join(self.fullDir, currentDir)
         if not os.path.exists(self.fullDir):
             os.makedirs(self.fullDir)
 
